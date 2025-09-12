0c011604fd84ab988ce900fd6741de46bdb45279
mm <mmmdev2003@gmail.com>
2025-09-12 08:41:27 +0300

    init

---
+++ internal/controller/http/handler/account/model.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+class AuthorizationBody(BaseModel):
+    account_id: int
+    two_fa_status: bool
+    role: str
+
+class AuthorizationResponse(BaseModel):
+    access_token: str
+    refresh_token: str
+
+class CheckAuthorizationResponse(BaseModel):
+    account_id: int
+    two_fa_status: bool
+    message: str
